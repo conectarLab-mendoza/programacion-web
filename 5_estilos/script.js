fa0f3db7c80a14e8c3c89b90ede7b42865d97076
Lucas Zarandón <86167490+lucaszhh@users.noreply.github.com>
2023-10-31 17:49:44 -0300

    Add files via upload

---
+++ 5_estilos/script.js
@@ -0,0 +1,8 @@
+function crearCarta() {
+    const nombre = prompt("Dime el nombre del pokémon")
+    const imagen = prompt("Dame una imagen del pokémon")
+    const descripcion = prompt("Describí el pokémon")
+    
+    
+    alert(nombre+imagen+descripcion)
+}
